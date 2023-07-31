d0f0aa9a235e78e01e2b4198972975d68aa52d06
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-07-31 14:59:34 +0800

    feat: add QR Code display

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.scss";
 function App() {
   return (
     <div className="app hvc">
-      <div className="card"></div>
+      <div className="card">
+        <div className="card__content stack">
+          <img src="/images/image-qr-code.png" title="qr-code" />
+        </div>
+      </div>
     </div>
   );
 }
