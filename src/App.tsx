b6f971c8eaef18b4cf6e81c3dbf4b99c7c3cf3ae
Ning Sheng Ying <ning_sheng_ying@outlook.com>
2023-07-31 15:00:50 +0800

    feat: include card text content

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
       <div className="card">
         <div className="card__content stack">
           <img src="/images/image-qr-code.png" title="qr-code" />
+
+          <div className="card__text-content">
+            <h3>Improve your front-end skills by buidling projects</h3>
+            <p>
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
         </div>
       </div>
     </div>
